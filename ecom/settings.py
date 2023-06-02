2562a78f47389cc10786673a117b2ff037fb48fb
Vissu0901 <viswanath.varikuti09@gmail.com>
2023-06-02 19:34:40 +0530

    postgresql deploy

--- ecom/settings.py
+++ ecom/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -83,11 +84,15 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+DATABASE_URL = 'postgres://online_shopping_django_react_db_user:aEkQ9v6ItYxYnGlaJ22GsmCeqpU0QTVH@dpg-chsv46ak728ud3mc07l0-a.oregon-postgres.render.com/online_shopping_django_react_db'
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    "default": dj_database_url.config(default=DATABASE_URL),
 }
 
 
